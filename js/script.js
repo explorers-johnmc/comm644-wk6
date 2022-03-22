f64cd1f7108017d42f62a0b099dc1a0985e1d7e8
JohnMc <jdmcdaniel@cox.net>
2022-03-21 23:46:45 -0700

    working, needs clean-up

--- js/script.js
+++ js/script.js
@@ -2,17 +2,28 @@
 console.log("JavaScript is running, as deferred ")
 
 
-const $js = getDocmentById(selection-request);
+// const $js = function(idRequest) { document.getDocmentById(idRequest); }
+//const $js = selector => document.getDocmentById(selector); 
+const $   = selector => document.querySelector(selector);
+let fmEmpFormAction  = document.getElementById("empForm");  
 
+console.log("#2 is running, as deferred ")
 //  OUTPUTS  
-/*
-//console.log(`
+function fnConsoleLogRecords(Event) {
+console.log("#24is running, as deferred ")
+Event.preventDefault();
+alert("we are here")
+console.log(`
 
-ID:        ${`  $js(inIdNumber).value `}      <br>
-Name:        $js(inFullName).value        <br>
-Extension:   $js(in4DigitExt).value       <br>
-Email:       $js(inEmail).value           <br>
-Department:  $js(inSelectionDepartment).value <br>
+ID:         ${ $("#inIdNumber").value   }      
+Name:       ${ $("#inFullName").value   }      
+Extension:  ${ $("#in4DigitExt").value  }      
+Email:      ${ $("#inEmail").value      }      
+Department: ${ $("#inSelectionDepartment").value } 
 `
 );
-*/
+}
+
+
+//event listeners attached tO form not button/submit; yet the event will be submit, bubbled up to the parent form
+fmEmpFormAction.addEventListener("submit", function(Event) {fnConsoleLogRecords(Event); }  );
