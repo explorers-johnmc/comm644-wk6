393923efc41ac4516a31e37c4b6cf31caa56853f
JohnMc <jdmcdaniel@cox.net>
2022-03-21 18:29:43 -0700

    re put

---
+++ js/script.js
@@ -0,0 +1,18 @@
+"use strict"
+console.log("JavaScript is running, as deferred ")
+
+
+const $js = getDocmentById(selection-request);
+
+//  OUTPUTS  
+/*
+//console.log(`
+
+ID:        ${`  $js(inIdNumber).value `}      <br>
+Name:        $js(inFullName).value        <br>
+Extension:   $js(in4DigitExt).value       <br>
+Email:       $js(inEmail).value           <br>
+Department:  $js(inSelectionDepartment).value <br>
+`
+);
+*/
