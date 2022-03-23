41fa784cb14d3bc64edf9650e5ca2291d5a7bf35
JohnMc <jdmcdaniel@cox.net>
2022-03-23 09:48:29 -0700

    converted to compact fat arrow functions

--- js/script.js
+++ js/script.js
@@ -1,29 +1,25 @@
 "use strict"
-console.log("JavaScript is running, as deferred ")
 
+// preserve $() for jQuery assignmnets;
+//const $() = 
+const $jsSel   = selector => document.querySelector(selector);
+const $jsId    = id       => document.getElementById(id);
 
-// const $js = function(idRequest) { document.getDocmentById(idRequest); }
-//const $js = selector => document.getDocmentById(selector); 
-const $   = selector => document.querySelector(selector);
-let fmEmpFormAction  = document.getElementById("empForm");  
+let fmEmpFormAction  = $jsId("empForm");  
 
-console.log("#2 is running, as deferred ")
 //  OUTPUTS  
 function fnConsoleLogRecords(Event) {
-console.log("#24is running, as deferred ")
 Event.preventDefault();
-alert("we are here")
+//alert("we are here, after preventDefault")  //At 1 point I had trouble having this triggered correctly; and page refreshed.
 console.log(`
 
-ID:         ${ $("#inIdNumber").value   }      
-Name:       ${ $("#inFullName").value   }      
-Extension:  ${ $("#in4DigitExt").value  }      
-Email:      ${ $("#inEmail").value      }      
-Department: ${ $("#inSelectionDepartment").value } 
-`
-);
+ID:         ${ $jsSel("#inIdNumber").value   }      
+Name:       ${ $jsSel("#inFullName").value   }      
+Extension:  ${ $jsSel("#in4DigitExt").value  }      
+Email:      ${ $jsSel("#inEmail").value      }      
+Department: ${ $jsSel("#inSelectionDepartment").value } 
+` );
 }
-
-
-//event listeners attached tO form not button/submit; yet the event will be submit, bubbled up to the parent form
-fmEmpFormAction.addEventListener("submit", function(Event) {fnConsoleLogRecords(Event); }  );
+//  EVENTS
+//event listeners attached to form not button/submit; yet the event will be submit, bubbled up to the parent form
+fmEmpFormAction.addEventListener("submit", Event => fnConsoleLogRecords(Event), false  ); // add the default flase just as a placeholder fo completeness
